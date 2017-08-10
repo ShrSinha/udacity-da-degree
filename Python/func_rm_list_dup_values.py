aca06db62f97377cb9acaedfaeed661fd9ff376d
ShrSinha <shruti.sinha@gmail.com>
2017-08-09 19:04:01 -0700

    feat:Return new list containing unique elements of original list.

---
+++ Python/func_rm_list_dup_values.py
@@ -0,0 +1,12 @@
+def func_rm_list_dup_values(list):
+	"""Return a new list containing unique elements of the original list. """
+	unique_list = []
+
+	for item in list:
+		if item not in unique_list:
+			unique_list.append(item)
+
+	return(unique_list)
+
+list = [1,1,2,3,4,5,5]
+print(func_rm_list_dup_values(list))			
