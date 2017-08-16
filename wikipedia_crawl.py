cf9735c88b35cf4bb4bb45521e92f2d7632f482f
ShrSinha <shruti.sinha@gmail.com>
2017-08-16 16:49:58 -0700

    refactor: Improve readability.

--- wikipedia_crawl.py
+++ wikipedia_crawl.py
@@ -2,22 +2,20 @@ import time,urllib,requests
 from bs4 import BeautifulSoup
 
 def wikipedia_crawl(initial_url,target_url="https://en.wikipedia.org/wiki/Philosophy",max_steps=25):
-    """
-    Follow the first URL from one page to next and see if the crawl ends up at 
+    """ Follow the first URL from one page to next and see if the crawl ends up at 
     target page within specified steps.
+    
     The URL should not be a citation etc. It should be part of page's content.
     The initial page is assumed to be different from target page.
 
-    initial_url = string. 
-    target_url = string.
-    max_steps = int.
+    initial_url -- string. 
+    target_url -- string.
+    max_steps -- int.
     """
 
     def find_first_url(url):
         # Function to find the page's first url, ignoring help and citations.
-
         link = None
-
         response = requests.get(url)
         html = response.text
         soup = BeautifulSoup(html, "html.parser")
@@ -54,8 +52,8 @@ def wikipedia_crawl(initial_url,target_url="https://en.wikipedia.org/wiki/Philos
     while continue_crawl(search_history, target_url, max_steps):
         # Function to implement crawl.
         # Function waits for 2 seconds before sending the next web page request.
-
         first_url = find_first_url(search_history[-1])
+
         if not first_url:
             print("Search has arrived at a web page with no links and is aborted.")
             break
@@ -63,5 +61,7 @@ def wikipedia_crawl(initial_url,target_url="https://en.wikipedia.org/wiki/Philos
         search_history.append(first_url)
 
         time.sleep(2) 
+
+
 
 wikipedia_crawl("https://en.wikipedia.org/wiki/Special:Random")        
