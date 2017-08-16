700c00347e8f568048bbf48053fabe5f91b8d22e
ShrSinha <shruti.sinha@gmail.com>
2017-08-15 18:51:54 -0700

    chore: Re-organize folders

---
+++ python-scrapbook/func_most_prolific_yr.py
@@ -0,0 +1,25 @@
+def func_most_prolific_yr(dict_discography):
+
+	temp_list =[]
+	temp_count = 0
+	
+	for key in dict_discography:
+		temp_list.append(dict_discography[key])
+		
+	for index in temp_list:
+	    if temp_list.count(index) > temp_count:
+	        temp_count = temp_list.count(index)
+	        temp_year = index
+	return temp_year	
+
+
+
+Beatles_Discography = {"Please Please Me": 1963, "With the Beatles": 1963, 
+    "A Hard Day's Night": 1964, "Beatles for Sale": 1964, "Twist and Shout": 1964,
+    "Help": 1965, "Rubber Soul": 1965, "Revolver": 1966,
+    "Sgt. Pepper's Lonely Hearts Club Band": 1967,
+    "Magical Mystery Tour": 1967, "The Beatles": 1968,
+    "Yellow Submarine": 1969 ,'Abbey Road': 1969,
+    "Let It Be": 1970}
+
+print(most_prolific(Beatles_Discography))  
